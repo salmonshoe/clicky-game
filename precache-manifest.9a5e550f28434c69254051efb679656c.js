816d1024978586f776ba262f4383014fc2f65389
salmonshoe <rafaelreynoso273@gmail.com>
2019-06-25 18:19:46 -0400

    Updates

---
+++ precache-manifest.9a5e550f28434c69254051efb679656c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39aa7e10ee75cb339d2a4b0b582253c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "02a2130a690ac4b4534d",
+    "url": "/clicky-game/static/css/main.f0c4d598.chunk.css"
+  },
+  {
+    "revision": "7e6d46910caa7284e0ff",
+    "url": "/clicky-game/static/js/2.647c2da8.chunk.js"
+  },
+  {
+    "revision": "02a2130a690ac4b4534d",
+    "url": "/clicky-game/static/js/main.7aba3082.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "555490b9d520c482eab54be89eaf8305",
+    "url": "/clicky-game/static/media/concert.555490b9.jpg"
+  }
+]);
